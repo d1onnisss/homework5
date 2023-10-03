d0cfc77b315e3404416c786e396c669b8cc87a02
d1onnisss <lim_al@auca.kg>
2023-10-03 22:04:45 +0600

    second commit

---
+++ hw.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main () {
+  fourthTask();
+}
+
+void firstTask() {
+  stdout.write("Введите число: ");
+
+  int range = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+
+  if (range <= 0) {
+    print("Диапозон должен быть больше 0");
+  }
+
+  int sum = 0;
+   for (int i = 0; i <= range; i++) {
+    sum += i;
+   }
+
+   print(sum);
+}
+
+void secondTask() {
+  stdout.write("Введите Ваш год: ");
+  int year = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+
+  if (year <= 0) {
+    print("Ошибка. Попробуйте заново");
+  }
+
+  if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    print("$year - високосный год");
+  } else {
+    print("$year - не високосный год");
+  }
+}
+
+void thirdTask() {
+  stdout.write("Введите Ваше число: ");
+  String num = stdin.readLineSync() ?? "";
+  int sum = 0;
+
+  for (int i = 0; i < num.length; i++) {
+    int? digit = int.tryParse(num[i]);
+    if (digit != null) {
+      sum += digit;
+    }
+  }
+
+  print(sum);
+}
+
+void fourthTask() {
+  stdout.write("Введите Ваше слово: ");
+  String word = stdin.readLineSync() ?? "";
+  List<String> reverse = []; 
+  List<String> reverseWord = []; 
+
+  for (int i = 0; i < word.length; i++) {
+    reverse.add(word[i]);
+  }
+
+reverseWord = reverse.reversed.toList();
+print(reverseWord.join(""));
+  
+}
+
